0830c0295f99c058b18ec5cd87789c2807f6615f
cris frulla <cris.frulla.com>
2014-10-23 16:41:42 -0400

    MathsNumbers with Ruby

---
+++ numbers.rb
@@ -0,0 +1,46 @@
+# Numbers Ruby test
+
+#puts 1+2
+
+#puts 5.0/3
+
+#puts 1.0 + 2.0
+#puts 2.0 * 3.0
+#puts 5.0 - 8.0
+#puts 9.0 / 2.0
+
+#puts 1+2
+#puts 2*3
+#puts 5-8
+#puts 9/2
+
+#puts 5 * (12-8) + -15
+
+#puts 98 + (59872 / (13*8)) * -52
+
+#How many hours are in a year?
+
+#puts 365.0 * 24.0
+#8760.0
+
+#puts 8760.0 * 10.0
+
+#how many minutes are in a decade?
+
+#puts (8760 * 60) * 10
+
+#puts ((365 * 24) * 60) * 10
+#5256000
+
+#how many seconds old are you?
+#puts ((24 * 60) * 60) * 32
+#2764800
+
+#If I am 1204 million seconds old, how old am I?
+
+# 1204000
+
+#puts (24 * 60) * 60 
+
+#puts 1204000 / 86400
+#13
