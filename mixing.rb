9b8b75646853451818ae50ed29a611f7cd62d697
cris frulla <cris.frulla.com>
2014-10-23 17:23:42 -0400

    Mixing with Ruby

---
+++ mixing.rb
@@ -0,0 +1,38 @@
+# Mixing with Ruby
+
+# var1 = 2
+# var2 = '5'
+
+# puts var1 + var2
+# TypeError
+
+# var1 = 2
+# var2 = '5'
+
+# puts var1.to_s + var2
+
+# var1 = 2
+# var2 = '5'
+
+# puts var1.to_s + var2
+# puts var1 + var2.to_i
+
+# string = .to_s
+# number = .to_i
+
+# 25
+# 7
+
+# puts 'Hello there, and what\'s your name?'
+# name = gets
+# puts 'Your name is ' + name + '?  What a lovely name!'
+# puts 'Pleased to meet you, ' + name + '.  :)'
+
+# puts ' '
+
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '?  What a lovely name!'
+puts 'Pleased to meet you, ' + name + '.  :)'
+
+
